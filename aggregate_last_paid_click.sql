2d95a0bb62aec04c12ba6d32365a28ead82007d4
Shprovar <132303568+Shprovar@users.noreply.github.com>
2023-09-06 21:31:35 +0300

    Add files via upload

--- aggregate_last_paid_click.sql
+++ aggregate_last_paid_click.sql
@@ -1,37 +1,63 @@
 --версия для загрузки, неокончательная
+with vk_daily as (
+    select
+        utm_source,
+        utm_medium,
+        utm_campaign,
+        DATE_TRUNC('day', campaign_date) as vk_campaign_date,
+        SUM(daily_spent) as total_vk_spent
+    from vk_ads
+    group by
+        DATE_TRUNC('day', campaign_date), utm_source, utm_medium, utm_campaign
+),
+ya_daily as (
+    select
+        utm_source,
+        utm_medium,
+        utm_campaign,
+        DATE_TRUNC('day', campaign_date) as ya_campaign_date,
+        SUM(daily_spent) as total_ya_spent
+    from ya_ads
+    group by
+        DATE_TRUNC('day', campaign_date), utm_source, utm_medium, utm_campaign
+)
 select
-    to_char(visit_date, 'YY-MM-DD') as visit_date,
+    TO_CHAR(visit_date, 'YY-MM-DD') as visit_date,
     shpro.utm_source,
     shpro.utm_medium,
     shpro.utm_campaign,
-    count(visitor_id) as visitors_count,
-    case
-        when vk.daily_spent is not null then vk.daily_spent
-        when ya.daily_spent is not null then ya.daily_spent
+    COUNT(visitor_id) as visitors_count,
+    COALESCE(total_ya_spent, 0) + COALESCE(total_vk_spent, 0) as total_cost,
+    COUNT(lead_id) as leads_count,
+    SUM(case
+        when closing_reason = 'Успешная продажа' or status_id = 142
+            then 1
         else 0
-    end as total_cost,
-    count(lead_id) as leads_count,
-    count(closing_reason) as purchases_count,
-    sum(amount) as revenue
+    end) as purchases_count,
+    SUM(amount) as revenue
 from shpro_last_paid1 as shpro
-left join vk_ads as vk
+left join vk_daily as vk
     on
-        date_trunc('day', shpro.visit_date)
-        = date_trunc('day', vk.campaign_date)
+        shpro.visit_date = vk.vk_campaign_date
         and shpro.utm_source = vk.utm_source
         and shpro.utm_medium = vk.utm_medium
         and shpro.utm_campaign = vk.utm_campaign
-left join ya_ads as ya
+left join ya_daily as ya
     on
-        date_trunc('day', shpro.visit_date)
-        = date_trunc('day', ya.campaign_date)
+        shpro.visit_date = ya.ya_campaign_date
         and shpro.utm_source = ya.utm_source
         and shpro.utm_medium = ya.utm_medium
         and shpro.utm_campaign = ya.utm_campaign
 where status_id = 142
-group by visit_date, 1, 2, 3, 4, vk.daily_spent, ya.daily_spent
+group by visit_date, 1, 2, 3, 4, total_vk_spent, total_ya_spent
 order by
-    purchases_count desc, visit_date asc, visitors_count desc, shpro.utm_source asc, shpro.utm_medium asc, shpro.utm_campaign asc;
+    purchases_count desc,
+    visit_date asc,
+    visitors_count desc,
+    shpro.utm_source asc,
+    shpro.utm_medium asc,
+    shpro.utm_campaign asc;
+
 
 
 
