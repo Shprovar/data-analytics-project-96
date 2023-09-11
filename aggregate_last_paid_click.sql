16c08e398868f843bc791423c6ad64d6221ef666
Shprovar <132303568+Shprovar@users.noreply.github.com>
2023-09-11 22:03:53 +0300

    Add files via upload

--- aggregate_last_paid_click.sql
+++ aggregate_last_paid_click.sql
@@ -1,4 +1,3 @@
---версия для загрузки, неокончательная
 with vk_daily as (
     select
         utm_source,
@@ -22,12 +21,11 @@ ya_daily as (
         DATE_TRUNC('day', campaign_date), utm_source, utm_medium, utm_campaign
 ), tab as(
 select
-    TO_CHAR(visit_date, 'YY-MM-DD') as visit_date,
+    DATE_TRUNC('day', visit_date) as visit_date,
     shpro.utm_source,
     shpro.utm_medium,
     shpro.utm_campaign,
     COUNT(visitor_id) as visitors_count,
-    COALESCE(total_ya_spent, 0) + COALESCE(total_vk_spent, 0) as total_cost,
     COUNT(lead_id) as leads_count,
     SUM(case
         when closing_reason = 'Успешная продажа' or status_id = 142
@@ -36,32 +34,37 @@ select
     end) as purchases_count,
     SUM(amount) as revenue
 from shpro_last_paid1 as shpro
+group by 1, 2, 3, 4),
+crab as(
+select
+    to_char(visit_date, 'YYYY-MM-DD') as visit_date,
+    visitors_count,
+    tab.utm_source,
+    tab.utm_medium,
+    tab.utm_campaign,
+    COALESCE(total_ya_spent, 0) + COALESCE(total_vk_spent, 0) as total_cost,
+    leads_count,
+    purchases_count,
+    revenue
+    from tab
 left join vk_daily as vk
     on
-        shpro.visit_date = vk.vk_campaign_date
-        and shpro.utm_source = vk.utm_source
-        and shpro.utm_medium = vk.utm_medium
-        and shpro.utm_campaign = vk.utm_campaign
+        tab.visit_date = vk.vk_campaign_date
+        and tab.utm_source = vk.utm_source
+        and tab.utm_medium = vk.utm_medium
+        and tab.utm_campaign = vk.utm_campaign
 left join ya_daily as ya
     on
-        shpro.visit_date = ya.ya_campaign_date
-        and shpro.utm_source = ya.utm_source
-        and shpro.utm_medium = ya.utm_medium
-        and shpro.utm_campaign = ya.utm_campaign
-group by visit_date, 2, 3, 4, total_cost
+        tab.visit_date = ya.ya_campaign_date
+        and tab.utm_source = ya.utm_source
+        and tab.utm_medium = ya.utm_medium
+        and tab.utm_campaign = ya.utm_campaign
 order by
     purchases_count desc,
     visit_date asc,
     visitors_count desc,
-    shpro.utm_source asc,
-    shpro.utm_medium asc,
-    shpro.utm_campaign asc)
-select *
-from tab
-order by purchases_count desc
-limit 15;
-
-
-
-
-
+    tab.utm_source asc,
+    tab.utm_medium asc,
+    tab.utm_campaign asc)
+select * from crab
+order by purchases_count desc limit 15;
