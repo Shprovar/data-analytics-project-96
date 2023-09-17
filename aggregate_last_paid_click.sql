a93a26720682ae55f064f51bf1a52569735200bd
Shprovar <132303568+Shprovar@users.noreply.github.com>
2023-09-17 14:38:22 +0300

    Add files via upload

--- aggregate_last_paid_click.sql
+++ aggregate_last_paid_click.sql
@@ -1,85 +1,55 @@
-with vk_daily as (
+with vk_ya as (
     select
         utm_source,
         utm_medium,
         utm_campaign,
-        DATE_TRUNC('day', campaign_date) as vk_campaign_date,
-        SUM(daily_spent) as total_vk_spent
+        campaign_date,
+        daily_spent
     from vk_ads
-    group by
-        DATE_TRUNC('day', campaign_date), utm_source, utm_medium, utm_campaign
-),
-ya_daily as (
+    union all
     select
         utm_source,
         utm_medium,
         utm_campaign,
-        DATE_TRUNC('day', campaign_date) as ya_campaign_date,
-        SUM(daily_spent) as total_ya_spent
+        campaign_date,
+        daily_spent
     from ya_ads
-    group by
-        DATE_TRUNC('day', campaign_date), utm_source, utm_medium, utm_campaign
-),
-tab_visitors as (
+), advertising_costs as(
+select DATE_TRUNC('day', campaign_date) as visit_date,
+ utm_source,
+        utm_medium,
+        utm_campaign,
+        sum(daily_spent) as total_cost
+        from vk_ya
+        group by 1, 2, 3, 4),
+tab as(
     select
-        DATE_TRUNC('day', visit_date) as visit_date,
-        shpro.utm_source,
-        shpro.utm_medium,
-        shpro.utm_campaign,
+        to_char(shpro.visit_date, 'YYYY-MM-DD') as visit_date,
         COUNT(visitor_id) as visitors_count,
-        SUM(case
-when closing_reason = 'Успешная продажа' or status_id = 142
-then 1
-else 0
-end) as purchases_count,
-SUM(amount) as revenue
-    from shpro_last_paid1 as shpro
-    group by 1, 2, 3, 4
-),
-tab_leads as (
-    select
-        DATE_TRUNC('day', visit_date) as visit_date,
         shpro.utm_source,
         shpro.utm_medium,
         shpro.utm_campaign,
-        COUNT(lead_id) as leads_count
-    from shpro_last_paid1 as shpro
-    where visit_date < created_at
-    group by 1, 2, 3, 4
-),
-crab as (
-    select
-        to_char(tv.visit_date, 'YYYY-MM-DD') as visit_date,
-        tv.visitors_count,
-        tv.utm_source,
-        tv.utm_medium,
-        tv.utm_campaign,
-        COALESCE(total_ya_spent, 0) + COALESCE(total_vk_spent, 0) as total_cost,
-        tl.leads_count, 
-        purchases_count,
-        revenue
-    from tab_visitors as tv
-    left join vk_daily as vk
-        on tv.visit_date = vk.vk_campaign_date
-        and tv.utm_source = vk.utm_source
-        and tv.utm_medium = vk.utm_medium
-        and tv.utm_campaign = vk.utm_campaign
-    left join ya_daily as ya
-        on tv.visit_date = ya.ya_campaign_date
-        and tv.utm_source = ya.utm_source
-        and tv.utm_medium = ya.utm_medium
-        and tv.utm_campaign = ya.utm_campaign
-    left join tab_leads as tl
-        on tv.visit_date = tl.visit_date
-        and tv.utm_source = tl.utm_source
-        and tv.utm_medium = tl.utm_medium
-        and tv.utm_campaign = tl.utm_campaign
+        total_cost,
+        leads_count,
+        SUM(case
+            when closing_reason = 'Успешная продажа' or status_id = 142
+                then 1
+            else 0
+        end) as purchases_count,
+        SUM(amount) as revenue
+    from public.shpro_last_paid1 as shpro
+    left join advertising_costs as adv
+        on shpro.visit_date = adv.visit_date
+        and shpro.utm_source = adv.utm_source
+        and shpro.utm_medium = adv.utm_medium
+        and shpro.utm_campaign = adv.utm_campaign
+    group by 1, 3, 4, 5, 6, 7
     order by purchases_count desc,
         visit_date asc,
         visitors_count desc,
-        tv.utm_source asc,
-        tv.utm_medium asc,
-        tv.utm_campaign asc
-)
-select * from crab
-order by purchases_count desc limit 15;
+        shpro.utm_source asc,
+        shpro.utm_medium asc,
+        shpro.utm_campaign asc)
+select * from tab
+order by purchases_count desc
+limit 15;
--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,14 +1,16 @@
 Шаг2 запрос для атрибуции лидов по модели Last Paid Click
-with tab as (
+WITH tab AS (
     select
         *,
         s.visitor_id as sessions_visitor_id,
         ROW_NUMBER()
             over (partition by lead_id order by s.visit_date desc)
         as click_rank
+        --,CASE WHEN s.visit_date < leads.created_at THEN leads.lead_id ELSE '0' END AS leads_tmp
     from sessions as s
-    left join leads on s.visitor_id = leads.visitor_id
+    left join leads on s.visitor_id = leads.visitor_id --AND s.visit_date <= leads.created_at
     where medium != 'organic'
+    AND (s.visit_date <= leads.created_at)
 )
 select
     sessions_visitor_id as visitor_id,
@@ -16,7 +18,7 @@ select
     source as utm_source,
     medium as utm_medium,
     campaign as utm_campaign,
-    lead_id,
+    count(lead_id) as leads_count,
     created_at,
     amount,
     closing_reason,
@@ -24,10 +26,11 @@ select
 from tab
 where
     click_rank = 1 and amount is not null
+   -- and visit_date <= created_at
+group by 1, 2, 3, 4, 5, created_at, amount, closing_reason, status_id
 order by
     amount desc,
     visit_date asc,
     utm_source asc,
     utm_medium asc,
-    utm_campaign asc
-limit 10;
+    utm_campaign asc;
