e3e1bbc302fed00ed597cab47e8e24d0d1e1e89c
Shprovar <132303568+Shprovar@users.noreply.github.com>
2023-10-01 19:31:22 +0300

    Update aggregate_last_paid_click.sql

--- aggregate_last_paid_click.sql
+++ aggregate_last_paid_click.sql
@@ -1,32 +1,31 @@
-with vk_ya as (
-    select
-        utm_source,
-        utm_medium,
-        utm_campaign,
-        campaign_date,
-        daily_spent
-    from vk_ads
-    union all
-    select
-        utm_source,
-        utm_medium,
-        utm_campaign,
-        campaign_date,
-        daily_spent
-    from ya_ads
-),
-
-advertising_costs as (
-    select
-        DATE_TRUNC('day', campaign_date) as visit_date,
-        utm_source,
-        utm_medium,
-        utm_campaign,
-        SUM(daily_spent) as total_cost
-    from vk_ya
-    group by 1, 2, 3, 4
-)
-
+with vk_ya as
+(SELECT
+    visit_date,
+    utm_source,
+    utm_medium,
+    utm_campaign,
+    SUM(daily_spent) as total_cost
+FROM(SELECT
+            DATE_TRUNC('day', campaign_date) as visit_date,
+            utm_source,
+            utm_medium,
+            utm_campaign,
+            daily_spent
+        FROM vk_ads
+        UNION ALL
+        SELECT
+            DATE_TRUNC('day', campaign_date) as visit_date,
+            utm_source,
+            utm_medium,
+            utm_campaign,
+            daily_spent
+        FROM ya_ads
+    ) as subquery
+GROUP BY
+    visit_date,
+    utm_source,
+    utm_medium,
+    utm_campaign)
 select
     TO_CHAR(shpro.visit_date, 'YYYY-MM-DD') as visit_date,
     COUNT(visitor_id) as visitors_count,
@@ -42,13 +41,13 @@ select
     end) as purchases_count,
     SUM(amount) as revenue
 from public.shpro_last_paid1 as shpro
-left join advertising_costs as adv
+left join vk_ya
     on
-        DATE_TRUNC('day', shpro.visit_date) = adv.visit_date
-        and shpro.utm_source = adv.utm_source
-        and shpro.utm_medium = adv.utm_medium
-        and shpro.utm_campaign = adv.utm_campaign
-group by 1, 3, 4, 5, 6
+        DATE_TRUNC('day', shpro.visit_date) = vk_ya.visit_date
+        and shpro.utm_source = vk_ya.utm_source
+        and shpro.utm_medium = vk_ya.utm_medium
+        and shpro.utm_campaign = vk_ya.utm_campaign
+group by 1, 3, 4, 5,6
 order by
     revenue desc nulls last,
     visit_date asc,
@@ -57,4 +56,3 @@ order by
     shpro.utm_medium asc,
     shpro.utm_campaign asc
 limit 15;
-
